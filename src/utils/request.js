50d6ec0cd1ae64094301526b9632f2c9eedd52bd
Huỳnh Minh Trí <mtrigenius@gmail.com>
2025-03-03 15:34:59 +0700

    Update Api

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,5 +1,6 @@
-const API_DOMAIN = "http://localhost:3002/";
+// const API_DOMAIN = "http://localhost:3002/";
 // const API_DOMAIN = "https://dummyjson.com/";
+const API_DOMAIN = "https://db-manage-products.vercel.app/";
 
 export const get = async (path) => {
   const response = await fetch(API_DOMAIN + path);
